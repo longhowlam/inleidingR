242d129a9b0ec598adcb88f379347709cf81a5ef
longhow lam <longhowlam@gmail.com>
2017-04-18 16:32:04 +0200

    CC

--- Oefeningen.R
+++ Oefeningen.R
@@ -17,12 +17,13 @@
 
 #### Avond sessie 3 ###########################################################
 
-
 library(readr)
-AUTO_TRADER_ALL_BEWERKT = read_delim(
-  "data/AUTO_TRADER_ALL_BEWERKT.csv", 
-  ";"
-)
+
+Restaurants = read_csv("data/Restaurants.csv")
+
+
+
+AUTO_TRADER_ALL_BEWERKT = read_delim( "data/AUTO_TRADER_ALL_BEWERKT.csv", ";")
 
 
 
