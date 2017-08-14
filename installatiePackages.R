23422fa8f6a219acbf8e4de4c2f53b1f652fdc55
longhow lam <longhowlam@gmail.com>
2017-08-14 07:56:55 +0200

    CC

--- installatiePackages.R
+++ installatiePackages.R
@@ -1,3 +1,23 @@
 ## installatie van packages die nodig zijn voor inleiding R
 
-install.packages()
+# Sessie 01
+install.packages(
+  c("readr", "readxl", "pryr", "RODBC")
+)
+
+# sessie 02
+install.packages(
+  c("tidyverse", "lubridate", "anytime","tidyr", "stringr", "forcats")
+)
+
+# sessie 03
+install.packages(
+  c("ggplot2" , "plotly", "grid", "ggvis", "leaflet", "visNetwork", "sunburstR", "devtools")
+)
+
+devtools::install_github("jeromefroe/circlepackeR")
+
+# sessie 04
+install.packages(
+  c("rpart", "glmnet", "ranger", "xgboost", "ROCR", "pROC", "titanic", "rattle", "mlr", "arules")  
+)
