87dd9ded852911763239b78025856d8bbcbb4ef2
longhowlam <longhowlam@gmail.com>
2019-01-30 15:18:59 +0100

    cheese

--- osm_test.R
+++ osm_test.R
@@ -1,20 +1,27 @@
-# maps and openstreetmaps in R
+############### maps and openstreetmaps in R ################################################
+
 library(tidyverse)
 library(osmdata)
 library(sf)
 library(ggmap)
-
+library(leaflet)
 # need latest dev version because of jpeg issues
 #devtools::install_github("dkahle/ggmap") 
 
-##########################################################
+######  restaurant data ####################################################
+
+restaurants <- readr::read_csv("data/Restaurants.csv")
 
 ## restaurants in amsterdam
 amsterdam = restaurants %>% 
   filter(
     plaats == "Amsterdam",
-    !is.na(aantalreviews)
+    !is.na(aantalreviews),
+    !is.na(LONGs),
+    !is.na(LATs)
   )
+
+## transform data to a sf object (Simple Features object: open standard in data format for GIS 
 projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 amsterdam <- st_as_sf(
   x = amsterdam,                         
@@ -29,16 +36,19 @@ amsterdam_map <- get_map(
 )
 
 ggmap(amsterdam_map) +
-  geom_sf(data = df,
-          inherit.aes =FALSE,
-          aes(colour = aantalreviews, fill = aantalreviews),
-          alpha = .65,
-          size = 1,
-          shape = 21)+
+  geom_sf(
+    data = amsterdam,
+    inherit.aes =FALSE,
+    aes(colour = aantalreviews, fill = aantalreviews),
+    alpha = .65,
+    size = 1,
+    shape = 21
+  )+
   labs(title = "restaurants in Amsterdam", x="",y="")
 
 
-############################################################
+######## Heel nederland ###################################################
+# duurt erg lang
 
 mad_map <- get_map(
   c(left = 3.3, bottom = 50.5, right = 7.3, top = 53.6902),
@@ -62,7 +72,7 @@ ggmap(mad_map) +
           shape = 21)+
   labs(title = "restaurants in Amsterdam", x="",y="")
 
-###############################################################
+########### POI ####################################################
 
 # Er is meer. Je kan PoI opvragen met osm
 
@@ -73,29 +83,45 @@ available_features()
 # en deze heeft weer tags. Die kan je tonen
 available_tags("amenity")
 
-#shops
+# shops
 available_tags("shop")
 
-# Querying PoI's
+#### Querying PoI's ###########################
+
+## Speed Cameras in Amsterdam
 q <- getbb("Amsterdam")%>%
   opq()%>%
-  #add_osm_feature("amenity", "restaurant")
   add_osm_feature("highway",	"speed_camera")
-
 out = osmdata_sf(q)
-pp = cinema$osm_points 
+speed = out$osm_points 
+
+## Kaartje met PoI's
+ggmap(amsterdam_map) +
+  geom_sf(data = speed,
+          inherit.aes =FALSE,
+          colour="#238443",
+          fill="#004529",
+          alpha = .95,
+          size = 4,
+          shape=21)+
+  labs(title = "fixed speed cameras")
 
 
-q <- getbb("Amsterdam")%>%
+
+## Cheese shops 
+
+out =  getbb("Amsterdam")%>%
   opq() %>%
-  add_osm_feature("shop")
+  add_osm_feature("shop") %>% 
+  osmdata_sf()
 
-out = osmdata_sf(q)
-pp = out$osm_points 
-ppp = pp %>%  select(name, addr.city, opening_hours, shop )
-typewinkels = ppp %>% group_by(shop) %>%  summarise(n=n())
+out2 = out$osm_points 
+out2 = out2 %>%  select(name, addr.city, opening_hours, shop )
 
-cheesewinkels = ppp %>% filter(shop == "cheese")
+# list type of shops
+typewinkels = out2 %>% group_by(shop) %>%  summarise(n=n())
+
+cheesewinkels = out2 %>% filter(shop == "cheese")
 
 # Plaats PoI's op een kaart
 
@@ -109,4 +135,13 @@ ggmap(amsterdam_map) +
           size = 2,
           shape=21)+
   labs(title = "cheese shops in Amsterdam")
+
+## Kan ook op leaflets
+leaflet(cheesewinkels) %>%
+  addTiles() %>%
+    addMarkers()
+
+leaflet(cheesewinkels) %>%
+  addTiles() %>%
+  addMarkers(icon = ~cheeseIcons)
 
